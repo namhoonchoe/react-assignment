7224af69104446013a4fe0c74c5bf06772523918
Namhoon Choe <sysynh2012@gmail.com>
2023-11-16 12:25:47 +0900

    [Fix] http => https

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const ApiKey = import.meta.env.VITE_REACT_APP_API_KEY
 //axios 인스턴스 생성
 
 const api = axios.create({
-  baseURL: 'http://apis.data.go.kr/6260000/fbusangmgtourinfo/',
+  baseURL: 'https://apis.data.go.kr/6260000/fbusangmgtourinfo/',
   params: {
     ServiceKey: ApiKey,
     pageNo:1,
